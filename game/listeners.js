a826f0dcd91bda81e9ed722f699f83969ae30071
Marco Pezzutti <marco.pezzutti@hotmail.it>
2014-08-12 12:12:32 +0200

    repo inizializzato

---
+++ game/listeners.js
@@ -0,0 +1,11 @@
+$('body').on('click', '#8gameButton', function() {
+    alert( $( this ).text() );
+})
+
+$('body').on('click', '#15gameButton', function() {
+    alert( $( this ).text() );
+})
+
+$('body').on('click', '#24gameButton', function() {
+    alert( $( this ).text() );
+})

1d93e551ce85bb45c3a05e227aab015ca0da1c8e
Marco Pezzutti <marco.pezzutti@hotmail.it>
2014-09-12 01:01:48 +0200

    modifiche barra laterale

--- game/listeners.js
+++ game/listeners.js
@@ -9,3 +9,34 @@ $('body').on('click', '#15gameButton', function() {
 $('body').on('click', '#24gameButton', function() {
     alert( $( this ).text() );
 })
+
+$('document').foundation({
+    slider: {
+        on_change: function(){
+
+        }
+    }
+});
+
+$('#cutoff-slider').change(function () {
+    var value = $(this).attr('data-slider');
+    $('#cutoff-value').text(value);
+})
+
+$('select').change(function () {
+    var value = $( "select option:selected" ).attr('value');
+    if(value == 'A') {
+        $('#opzioniA').show();
+        $('#opzioniIDA').hide();
+    }
+    else {
+        if(value == 'IDA') {
+            $('#opzioniIDA').show();
+            $('#opzioniA').hide();
+        }
+        else {
+            $('#opzioniIDA').hide();
+            $('#opzioniA').hide();
+        }
+    }
+});
